8452bae39739d090c02bfc28d7c74755820037d6
flanr <flanr@live.se>
2014-01-21 11:14:14 +0100

    fix main

--- Prototype/main.cpp
+++ Prototype/main.cpp
@@ -1,6 +1,16 @@
 // main.cpp
 
+#include <tchar.h>
+#include <SDL.h>
+#pragma comment (lib,"sdl2.lib")
+#pragma comment (lib,"sdl2main.lib")
 
 
 
 
+int _tmain(int argc,int *argv[])
+{
+
+
+	return 0;
+}
